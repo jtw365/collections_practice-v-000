4a7e6176b88f9f9d1da62645b9ff2549ae2e55ae
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-24 22:55:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
+def sort_array_asc(array)
+  array.sort {|x, y| x <=> y}
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|word| word.downcase}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array[1] = array[2]
+end
+
+def find_a(array)
+  array.detect do
+
+  end
+end
+
+def sum_array(array)
+  array.inject(0) {|sum, x| sum += x}
+end
